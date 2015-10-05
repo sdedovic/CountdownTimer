dcc631bb0f297b2e9c6919e1c5ae6d8365ce3b69
Stevan Dedovic <stevan@dedovic.com>
2015-10-04 19:20:51 -0500

    added dist stuff

---
+++ public/js/dist.js
@@ -0,0 +1,16 @@
+'use strict';
+
+$(document).ready(function () {
+    var num = 45;
+    var timer = $('#timer');
+    var tick = function tick() {
+        if (num > 0) {
+            num = num - 1;
+            timer.text(num);
+        } else {
+            timer.replaceWith('<iframe width="560" height="315" src="https://www.youtube.com/embed/EyoutEHpPAU?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+            clearInterval(counter);
+        }
+    };
+    var counter = setInterval(tick, 1000);
+});
